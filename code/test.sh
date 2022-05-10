0691076f0cd64f2c3432f687a39c7a54311807e2
ShareChatAI <99791662+ShareChatAI@users.noreply.github.com>
2022-05-10 20:34:31 +0530

    Testing script

--- code/test.sh
+++ code/test.sh
@@ -1,14 +1,14 @@
 #mode="vs_as"
-mode="vs_2as" # Video + Both Audio
-#mode="vs" # Only video
+#mode="vs_2as" # Video + Both Audio
+mode="vs" # Only video
 #mode="as" # Only audio
 #mode="2as" # Both audio
-featloc="/mnt/disks/3massiv/resnext/"
-audio_featloc="/mnt/disks/3massiv/clsril/"
-audio_featloc_second="/mnt/disks/3massiv/vggish/feats/"
+featloc="./features/3massiv_resnext50/"
+audio_featloc="./features/3massiv_clsril/"
+audio_featloc_second="./features/3massiv_vggish/feats/"
 phase="test"
-test_file_path="../data/3massiv_test_dummy.csv"
-model_path="path-to-trained-model"
+test_file_path="../data/3massiv_concept_test.csv"
+model_path="path-to-checkpoint"
 
 CUDA_VISIBLE_DEVICES=0 python3 run_main.py --gpus -1 \
 	--test_file $test_file_path \
